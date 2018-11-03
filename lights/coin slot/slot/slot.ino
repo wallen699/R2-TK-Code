1fd8e82894299e9503d590ea3ec8367023a47dc2
Wayne Allen <wayne@e-stim.co.uk>
2018-11-03 23:15:55 +0000

    Update lights/coin slot/slot/slot.ino
    Added Colin Slot and Vent Controls

--- lights/coin slot/slot/slot.ino
+++ lights/coin slot/slot/slot.ino
@@ -3,7 +3,8 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 1
+#define VENT_PIN 9
+#define SLOT_PIN 8
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -13,7 +14,16 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, PIN, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel vents = Adafruit_NeoPixel(2, VENT_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel slot = Adafruit_NeoPixel(6, SLOT_PIN, NEO_GRB + NEO_KHZ800);
+
+uint32_t magenta = vents.Color(255, 0, 255);
+uint32_t red = vents.Color(255, 0, 0);
+uint32_t green = vents.Color(0, 255, 0);
+uint32_t blue = vents.Color(0, 0, 255);
+uint32_t yellow = vents.Color(255, 255, 0);
+
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -28,76 +38,124 @@ void setup() {
   // End of trinket special code
 
 
-  strip.begin();
-  strip.setBrightness(128);
-  strip.show(); // Initialize all pixels to 'off'
+  vents.begin();
+  vents.setBrightness(255);
+  vents.show(); // Initialize all pixels to 'off'
+  
+  slot.begin();
+  slot.setBrightness(128);
+  slot.show(); // Initialize all pixels to 'off'
 }
 //R B W
 
-void loop() {
+void loop() 
+{
   // Some example procedures showing how to display to the pixels:
-  Dot(strip.Color(128, 0, 128), 200); // Red
-  DotA(strip.Color(128, 0, 0), 200); // Red
-  DotA(strip.Color(0, 128, 0), 200); // Red
-  DotA(strip.Color(0, 0, 128), 200); // Red
+  //DotA(strip.Color(128, 0, 0), 200); // Red 
+ // vents_off();
+ // slot_off();
+ // delay(500);
+  vents_colour(red,green);
+  slot_dot_up(blue,50);
+  slot_dot_down(blue,50);
+
+//  delay(500);
   
 }
 
-// Fill the dots one after the other with a color
-void Dot(uint32_t c, uint8_t wait)
+void vents_colour(uint32_t tcolour,uint32_t bcolour)
 {
-  for(uint16_t i=0; i<strip.numPixels(); i++)
+   vents.setPixelColor(0, tcolour);
+    vents.setPixelColor(1, bcolour);
+   
+    //Display
+    vents.show();
+}
+
+
+void vents_off()
+{
+       
+   vents.setPixelColor(0, 0);
+    vents.setPixelColor(1, 0);
+   
+    //Display
+    vents.show();
+}
+
+void slot_off()
+{
+    //Clear All pixels
+    for (uint16_t ii=0; ii < slot.numPixels(); ii++)
+    {
+        slot.setPixelColor(ii, 0);        //turn every  pixel off
+    }
+    //Display
+    slot.show();
+}
+
+void slot_colour(uint32_t colour)
+{
+    //Set All pixels
+    for (uint16_t ii=0; ii < slot.numPixels(); ii++)
+    {
+        slot.setPixelColor(ii, colour);        //turn every  pixel off
+    }
+    //Display
+    slot.show();
+}
+
+// Fill the dots one after the other with a color
+void slot_dot_down(uint32_t c, uint8_t wait)
+{
+  for(uint16_t i=0; i<slot.numPixels(); i++)
   {
     //Clear All pixels
-    for (uint16_t ii=0; ii < strip.numPixels(); ii++)
-    {
-        strip.setPixelColor(ii, 0);        //turn every  pixel off
-    }
+    slot_off();
     
     //Set Pixel
-    strip.setPixelColor(i, c);
+    slot.setPixelColor(i, c);
     
     //Display
-    strip.show();
+    slot.show();
     
     delay(wait);
   }
 }
 
+// Fill the dots one after the other with a color
+void slot_dot_up(uint32_t c, uint8_t wait)
+{
+  for(uint16_t i=slot.numPixels()+1;i>0; i--)
+  {
+    //Clear All pixels
+    slot_off();
+    
+    //Set Pixel
+    slot.setPixelColor(i-1, c);
+    
+    //Display
+    slot.show();
+    
+    delay(wait);
+  }
+}
 void DotA(uint32_t c, uint8_t wait)
 {
-  for(uint16_t i=strip.numPixels()+1; i>0; i--)
+  //for(uint16_t i=strip.numPixels()+1; i>0; i--)
   {
     //Clear All pixels
-    for (uint16_t ii=0; ii < strip.numPixels(); ii++)
+  //  for (uint16_t ii=0; ii < strip.numPixels(); ii++)
     {
-        strip.setPixelColor(ii, 0);        //turn every  pixel off
+   //     strip.setPixelColor(ii, 0);        //turn every  pixel off
     }
     
     //Set Pixel
-    strip.setPixelColor(i-1, c);
+   // strip.setPixelColor(i-1, c);
     
     //Display
-    strip.show();
+    //strip.show();
     
-    delay(wait);
+  //  delay(wait);
   }
-}
-
-
-
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
